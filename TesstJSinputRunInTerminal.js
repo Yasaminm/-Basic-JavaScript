5b878885ec847750731467f6cdac847db8562e03
Yasamin Mustafa du <yasaminmustafadu@MacBook-Pro-von-Yasamin.local>
2022-06-24 12:36:19 +0200

    Testing Code Runner for HTML in Chrome and JS in Terminal, JS Prompt

---
+++ TesstJSinputRunInTerminal.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+var fname = prompt("Enter your First Name: ");
+var lname = prompt("Enter your Last Name: ");
+
+console.log("Your Full Name is: ", fname, lname);
