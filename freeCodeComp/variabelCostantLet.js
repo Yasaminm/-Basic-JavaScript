b24c3c2971c1cce4ce431c7a084a506a94e51fec
Yasamin Mustafa du <yasaminmustafadu@MacBook-Pro-von-Yasamin.local>
2022-07-16 21:29:57 +0200

    Diff between const and let variable declaration

---
+++ freeCodeComp/variabelCostantLet.js
@@ -0,0 +1,4 @@
+const FCC = "freeCodeCamp"; // constant variable is read only and can not be reassigned
+let fact = "is cool!"; // ...
+fact = "is awesome!";
+console.log(FCC, fact) // console output: freeCodeCamp is awesome!
